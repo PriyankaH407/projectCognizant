148acbb9242081ecd52675a06d8a519949ccd2e3
PriyankaH407 <102574419+PriyankaH407@users.noreply.github.com>
2024-04-04 09:22:18 +0530

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEcommerceAdmin.App_Start
+{
+    public class FilterConfig : Controller
+    {
+        // GET: FilterConfig
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
